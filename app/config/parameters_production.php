a069dca7d4524afc0d58f3cd78d901b680c092c0
Nahuel Lattessi <nahuel.lattessi@ventas-privadas.com>
2015-06-07 19:14:12 -0300

    parameters

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -7,6 +7,7 @@ $container->setParameter('database_host', $url['host']);
 $container->setParameter('database_port', $url['port']);
 $container->setParameter('database_name', $url['name']);
 $container->setParameter('database_user', $url['user']);
+$container->setParameter('database_password', $url['pass']);
 $container->setParameter('secret', getenv('SECRET'));
 $container->setParameter('locale', 'en');
 $container->setParameter('mailer_transport', null);
