30b3bb45c9ba1f557571e67611456c8bd34a8c63
Nahuel Lattessi <nahuel.lattessi@ventas-privadas.com>
2015-07-15 15:05:22 -0300

    fix heroku

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -9,7 +9,7 @@
 // $container->setParameter('database_user', $db['user']);
 // $container->setParameter('database_password', $db['pass']);
 $container->setParameter('secret', getenv('SECRET'));
-// $container->setParameter('locale', 'en');
+ $container->setParameter('locale', 'en');
 // $container->setParameter('mailer_transport', null);
 // $container->setParameter('mailer_host', null);
 // $container->setParameter('mailer_user', null);
