b6121cd031ce45aa27998adb5fb258f515faa6bc
Nahuel Lattessi <nahuel.lattessi@ventas-privadas.com>
2015-07-15 15:04:02 -0300

    fix heroku

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -1,14 +1,14 @@
 <?php
 
-$db = parse_url(getenv('DATABASE_URL'));
+//$db = parse_url(getenv('DATABASE_URL'));
 
-$container->setParameter('database_driver', 'pdo_pgsql');
-$container->setParameter('database_host', $db['host']);
-$container->setParameter('database_port', $db['port']);
-$container->setParameter('database_name', $db['name']);
-$container->setParameter('database_user', $db['user']);
-$container->setParameter('database_password', $db['pass']);
-// $container->setParameter('secret', getenv('SECRET'));
+// $container->setParameter('database_driver', 'pdo_pgsql');
+// $container->setParameter('database_host', $db['host']);
+// $container->setParameter('database_port', $db['port']);
+// $container->setParameter('database_name', $db['name']);
+// $container->setParameter('database_user', $db['user']);
+// $container->setParameter('database_password', $db['pass']);
+$container->setParameter('secret', getenv('SECRET'));
 // $container->setParameter('locale', 'en');
 // $container->setParameter('mailer_transport', null);
 // $container->setParameter('mailer_host', null);
