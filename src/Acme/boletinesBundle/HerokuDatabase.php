513d11844f73f3c929bff63f028b7dda7f058ac2
Nahuel Lattessi <nahuel.lattessi@ventas-privadas.com>
2015-07-15 12:54:21 -0300

    fix heroku postgresql

---
+++ src/Acme/boletinesBundle/HerokuDatabase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Acme\BoletinesBundle;
+
+use Composer\Script\Event;
+
+class HerokuDatabase
+{
+    public static function populateEnvironment(Event $event)
+    {
+        $url = getenv("DATABASE_URL");
+
+        if ($url) {
+            $url = parse_url($url);
+            putenv("DATABASE_DRIVER=pdo_pgsql");
+            putenv("DATABASE_HOST={$url['host']}");
+            putenv("DATABASE_PORT={$url['port']}");
+            putenv("DATABASE_NAME={$url['name']}");
+            putenv("DATABASE_USER={$url['user']}");
+            putenv("DATABASE_PASSWORD={$url['pass']}");
+        }
+
+        $io = $event->getIO();
+
+        $io->write("DATABASE_URL=".getenv("DATABASE_URL"));
+    }
+}
