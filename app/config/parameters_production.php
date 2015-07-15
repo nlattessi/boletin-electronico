2660e5d807bfd8ee08339db23ad421143d986d0d
Nahuel Lattessi <nahuel.lattessi@ventas-privadas.com>
2015-07-15 13:26:42 -0300

    fix heroku postgresql

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -3,14 +3,14 @@
 $db = parse_url(getenv('DATABASE_URL'));
 
 $container->setParameter('database_driver', 'pdo_pgsql');
-$container->setParameter('database_host', $url['host']);
-$container->setParameter('database_port', $url['port']);
-$container->setParameter('database_name', $url['name']);
-$container->setParameter('database_user', $url['user']);
-$container->setParameter('database_password', $url['pass']);
-$container->setParameter('secret', getenv('SECRET'));
-$container->setParameter('locale', 'en');
-$container->setParameter('mailer_transport', null);
-$container->setParameter('mailer_host', null);
-$container->setParameter('mailer_user', null);
-$container->setParameter('mailer_password', null);
+$container->setParameter('database_host', $db['host']);
+$container->setParameter('database_port', $db['port']);
+$container->setParameter('database_name', $db['name']);
+$container->setParameter('database_user', $db['user']);
+$container->setParameter('database_password', $db['pass']);
+// $container->setParameter('secret', getenv('SECRET'));
+// $container->setParameter('locale', 'en');
+// $container->setParameter('mailer_transport', null);
+// $container->setParameter('mailer_host', null);
+// $container->setParameter('mailer_user', null);
+// $container->setParameter('mailer_password', null);
