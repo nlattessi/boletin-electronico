f258bc3fb90f59ca12172dd13b2175d463127625
Facundo <fclarat@hotmail.com>
2016-01-22 01:34:30 -0300

    historico funcioando

---
+++ src/Acme/boletinesBundle/Controller/HistoricController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Acme\boletinesBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class HistoricController extends Controller
+{
+
+    public function onAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $session->set('historic', 'on');
+
+        return new RedirectResponse($this->generateUrl('home'));
+    }
+
+    public function offAction()
+    {
+      $request = $this->getRequest();
+      $session = $request->getSession();
+      $session->set('historic', 'off');
+
+      return new RedirectResponse($this->generateUrl('home'));
+    }
+
+    public function downYearAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $year = $session->get('year') - 1;
+        $session->set('year', $year);
+        $start = mktime(0, 0, 0, 1, 1, $year);
+        $end = mktime(0, 0, 0, 12, 31, $year);
+        $session->set('startYear', date('Y-m-d H:i:s', $start));
+        $session->set('endYear', date('Y-m-d H:i:s', $end));
+
+        return new RedirectResponse($this->generateUrl('home'));
+    }
+
+    public function upYearAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $year = $session->get('year') + 1;
+        $session->set('year', $year);
+        $start = mktime(0, 0, 0, 1, 1, $year);
+        $end = mktime(0, 0, 0, 12, 31, $year);
+        $session->set('startYear', date('Y-m-d H:i:s', $start));
+        $session->set('endYear', date('Y-m-d H:i:s', $end));
+
+        return new RedirectResponse($this->generateUrl('home'));
+    }
+}
